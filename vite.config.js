18e798a3f4f6a83cc077f256477ab7bdc0789e75
Nuhayd12 <nuhayd.m.k@gmail.com>
2024-10-09 17:16:28 +0530

    config.js updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: '0.0.0.0', // Bind to all available IPs, necessary for Render
+    port: parseInt(process.env.PORT) || 4173, // Use the PORT environment variable from Render or default to 4173
+  },
+});
